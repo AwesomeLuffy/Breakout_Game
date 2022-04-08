43969d322b8445435c889029423f8c8ebc74db0b
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-08 19:40:35 -0400

    Restructure Game Management Sytem

---
+++ Breakout Game/Game/Game.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Breakout_Game.Game{
+    internal sealed class Game : GameBase{
+        private static Game _gmInstance = null;
+        private readonly GameWindow _gameWindow;
+        
+        internal static Game GetInstance (GameWindow gw){
+            //?? -> is null
+            return Game._gmInstance ?? (Game._gmInstance = new Game(gw));
+        }
+        
+        private Game(GameWindow gameWindow) : base(gameWindow){
+            this._gameWindow = gameWindow;
+            this.Start();
+        }
+
+        private void Start(){
+            EventListener.InitEventListener();
+            this._gameWindow.Load += this.Load;
+            this._gameWindow.UpdateFrame += this.Update;
+            this._gameWindow.RenderFrame += this.Render;
+            this._gameWindow.Resize += base.Resize;
+
+            this._gameWindow.MouseDown += EventListener.MouseDown;
+            this._gameWindow.Run(1.0/60.0);
+        }
+
+        private void Load(object sender, EventArgs e){
+            //TODO
+            GL.ClearColor(.75f, .75f, .75f, 1.0f);
+            GL.Enable(EnableCap.Texture2D);
+        }
+
+        private void Update(object sender, EventArgs e){
+        }
+
+        private void Render(object sender, EventArgs e){
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            
+            
+            this._gameWindow.SwapBuffers();
+        }
+
+        public static void CallEvent(Event e){ //If an event called ...
+        }
+        
+        
+
+    }
+}
---
+++ Breakout Game/Game/GameBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Breakout_Game.Game{
+    public abstract class GameBase{
+        private readonly GameWindow GameWindow;
+
+        protected GameBase(GameWindow gameWindow){
+            this.GameWindow = gameWindow;
+        }
+
+        protected void Resize(object sender, EventArgs eventArgs){
+            GL.Viewport(0, 0, this.GameWindow.Width, this.GameWindow.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-300.0, 300.0, -150.0, 150, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+    }
+}
