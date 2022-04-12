aefbaa2361e57a48012c3cac67889d3f1517f355
KISNOC <robinmcd56@gmail.com>
2022-04-12 18:48:35 -0400

    AudioManager base structure

--- Breakout Game/Game/Audio/AudioManager.cs
+++ Breakout Game/Game/Audio/AudioManager.cs
@@ -1,5 +1,31 @@
-﻿namespace Breakout_Game.Audio{
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Breakout_Game.Audio{
     public static class AudioManager{
-        
+        private static readonly IList<Audio> Audios;
+
+        public static readonly Audio TestSoundLoop = new Audio("audioTest.wav", true);
+        public static readonly Audio TestSound = new Audio("audio2Test.wav");
+
+
+        static AudioManager()
+        {
+            Audios = new List<Audio>()
+            {
+                TestSoundLoop,
+                TestSound
+            };
+        }
+
+        /// <summary>
+        /// init method, called to init the static constructor to not instanciate it when we need it and not create lag
+        /// </summary>
+
+        public static void init(){ }
+        public static bool isAnyAudioPlaying()
+        {
+            return Audios.Any(sound => sound.isPlaying());
+        }
     }
 }
