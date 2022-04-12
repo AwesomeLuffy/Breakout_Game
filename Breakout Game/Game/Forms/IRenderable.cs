7a002e826f06fbad0ead5237f34be468e6e9fed1
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-12 18:08:51 -0400

    Creating IRenderable & IUpdatable interface

---
+++ Breakout Game/Game/Forms/IRenderable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Breakout_Game.Forms{
+    /// <summary>
+    /// Interface to implement the Draw method
+    /// </summary>
+    internal interface IRenderable{
+        void Draw();
+    }
+
+    /// <summary>
+    /// Interface to implement the Draw And Update method
+    /// </summary>
+    internal interface IUpdatable : IRenderable{
+        void Update();
+    }
+}
