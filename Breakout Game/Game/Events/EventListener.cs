6f9a96b7f16519ca7a2752f57a59dcdba0804760
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-18 06:06:43 -0400

    EventListener test file

--- Breakout Game/Game/Events/EventListener.cs
+++ Breakout Game/Game/Events/EventListener.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Breakout_Game.Game.Forms;
 
 namespace Breakout_Game.Game.Events{
     public static class EventListener{
 
-        public static void InitEventListener(){ }
+        public static void InitEventListener(){
+            CreateFormEvent.Handler += OnFormCreate;
+        }
 
         
         public static void OnTestEv(object sender, EventArgs e){
@@ -12,7 +15,15 @@ namespace Breakout_Game.Game.Events{
 
         public static void OnFormCreate(object sender, EventArgs e){
             CreateFormEvent form = sender as CreateFormEvent;
-            if (form != null) Console.WriteLine("Form created !" + form.GetTypeInstance());
+            if (form != null) {
+                if (form.GetForm() is Brick) {
+                    Brick brick = form.GetForm() as Brick;
+                    if (brick != null) {
+
+                    }
+                }
+            }
+ 
         }
     }
 }
