a1be9c9bda2b3e762fc125f649d1e568776b8446
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-18 02:35:45 -0400

    Implementing editable texture system

--- Breakout Game/Game/Forms/BaseForm.cs
+++ Breakout Game/Game/Forms/BaseForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Breakout_Game.Game.Events;
 using Breakout_Game.Game.Texture;
@@ -9,20 +10,23 @@ using OpenTK.Graphics.OpenGL;
 namespace Breakout_Game.Game.Forms{
     internal abstract class BaseForm : IUpdatable{
         #region Attributes
+        
+        private static Dictionary<string, int> _ressourceLoaderId = new Dictionary<string, int>();
+
 
         private List<Vector2> _coordinates; //Texture
         protected List<Vector2> _points; //Form Position
         private Vector3 _colorRGB;
 
-        private readonly int textureId;
+
+        private int textureId;
 
         #endregion
 
         protected BaseForm(List<Vector2> points, string textureName){
             if (!(points.Count() <= 4)) return;
             
-            this.textureId = RessourceLoader.GenId();
-            RessourceLoader.LoadTexture(this.textureId, textureName);
+            this.textureId = BaseForm.CheckAndLoadTexture(textureName);
             
             this._points = points;
 
@@ -35,8 +39,24 @@ namespace Breakout_Game.Game.Forms{
 
             this._colorRGB = new Vector3(.0f, .5f, .8f);
             
-            Breakout_Game.Game.Game.CallEvent(new CreateFormEvent(this));
+            Game.CallEvent(new CreateFormEvent(this));
 
+        }
+
+        private static int CheckAndLoadTexture(string textureName){
+            int textId;
+            
+            if(!(_ressourceLoaderId.ContainsKey(textureName))) {
+                textId = RessourceLoader.GenId();
+                BaseForm._ressourceLoaderId[textureName] = textId;
+            }
+            else {
+                textId = BaseForm._ressourceLoaderId[textureName];
+            }
+            
+            RessourceLoader.LoadTexture(textId, textureName);
+
+            return textId;
         }
 
         #region Getter
@@ -47,6 +67,14 @@ namespace Breakout_Game.Game.Forms{
         #endregion
 
         #region Updating
+
+        public virtual void ChangeTexture(string textureName){
+            if (!(this is IEditableTexture)) {
+                Console.WriteLine("This Form is not Editable !");
+                return;
+            }
+            this.textureId = BaseForm.CheckAndLoadTexture(textureName);
+        }
 
         public abstract void Update();
 
