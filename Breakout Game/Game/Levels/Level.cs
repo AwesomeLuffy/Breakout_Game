3cdda5a299640132841fb8eaecc2c1aa679b2aa6
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-18 06:07:42 -0400

    Adding Level System

---
+++ Breakout Game/Game/Levels/Level.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Breakout_Game.Game.Forms;
+using OpenTK;
+
+namespace Breakout_Game.Game.Levels{
+    internal class Level{
+        
+        public const int MaxBrickInARow = 9;
+        public const int MaxBrickInColumn = 6;
+        
+        private const float HorizontalGap = 10;
+        private const float VerticalGap = 5;
+        private const float StartX = -270;
+        private const float StartY = 140;
+
+        public int NumberOfBricks{ get; set; }
+        
+        internal List<List<Brick>> bricks = new List<List<Brick>>();
+        
+        public Level(List<List<bool>> emplacement){
+            this.InitList();
+            
+            Vector2 originPoint = new Vector2(StartX, StartY);
+            for (int i = 0; i < emplacement.Count; i++) {
+
+                for (int j = 0; j < emplacement[i].Count; j++) {
+                    if (emplacement[i][j]) {
+                        
+                        this.bricks[i].Insert(j, new Brick(originPoint,
+                        (byte) ((j == MaxBrickInARow - 1 || j == 0) ? 4 :
+                            ((j == MaxBrickInARow - 2 || j == 1) ? 3 :
+                                ((j == MaxBrickInARow - 3 || j == 2) ? 2 : 1)))
+                        ));
+                    }
+
+                    originPoint.X += Brick.LenghtBrick + HorizontalGap;
+                }
+                originPoint.X = StartX;
+
+
+                originPoint.Y -= Brick.HeightBrick + VerticalGap;
+            }
+        }
+
+        private void InitList(){
+            for (int i = 0; i < MaxBrickInARow + 1; i++) {
+                this.bricks.Insert(i, new List<Brick>());
+            }
+        }
+    }
+}
--- Breakout Game/Game/Levels/LevelManager.cs
+++ Breakout Game/Game/Levels/LevelManager.cs
@@ -1,5 +1,31 @@
-﻿namespace Breakout_Game.Game.Levels{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Breakout_Game.Game.Levels{
     public static class LevelManager{
         
+        /*LEFT/RIGHT/TOP Extremity -> Lvl 4 - Indestructible
+         *First LEFT/RIGHT/TOP Extremity -> Lvl 3
+         *Second LEFT/RIGHT/TOP Extremity -> Lvl 2
+         *Else -> Lvl 1
+         */
+
+        private static List<List<bool>> _emplacement = Enumerable.Range(0, Level.MaxBrickInColumn).Select(i =>
+            Enumerable.Repeat(true, Level.MaxBrickInARow).ToList()).ToList();
+
+        private static List<Level> _levels = new List<Level>();
+
+
+        internal static void CreateFirstLevel(){
+            _levels.Add(new Level(_emplacement));
+        }
+
+        internal static Level GetFirstLevel(){
+            return _levels[0] ?? throw new Exception("Level not initilized");
+        }
+
+
+
     }
 }
