7e47949ea0bc5132249e13f048fcc13917326f19
KISNOC <robinmcd56@gmail.com>
2022-04-19 16:21:20 -0400

    Adding some text

---
+++ Breakout Game/Game/Text/TextManager.cs
@@ -0,0 +1,34 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout_Game.Game.Text
+{
+    public static class TextManager
+    {
+        private static IList<Text> Texts;
+
+        public static readonly Text TestText = new Text(
+                220,
+                25, 
+                "TXT HERE", 
+                Color.LightGray, 
+                new Font(FontFamily.GenericSansSerif, 11), 
+                new SolidBrush(Color.Red), 
+                new PointF(0.0f, 0.0f)
+            );
+
+        static TextManager()
+        {
+            Texts = new List<Text>()
+            {
+                TestText
+            };
+        }
+        public static void init() { }
+    }
+}
