0423b4293b95b0fbc4d9b3a622fe9e3e88ecfcc1
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-19 18:48:30 -0400

    UserControl -> OnKeyDown : Right & Left

--- Breakout Game/Game/UserInterac/UserControl.cs
+++ Breakout Game/Game/UserInterac/UserControl.cs
@@ -1,5 +1,30 @@
-﻿namespace Breakout_Game.UserInterac{
-    public static class UserControl{
+﻿using System;
+using Breakout_Game.Game;
+using Breakout_Game.Game.Forms;
+using Breakout_Game.Game.Utils;
+using OpenTK.Input;
+
+namespace Breakout_Game.Game.UserInterac{
+    internal static class UserControl{
+
+        internal static void OnKeyDown(object sender, KeyboardKeyEventArgs args){
+            switch (args.Key) {
+                case Key.Left:
+                    foreach (var renderable in Game.Renderables) {
+                        if (renderable is Racket racket) {
+                            racket.SetDirection(Direction.Left).Update();
+                        }
+                    }
+                    break;
+                case Key.Right:
+                    foreach (var renderable in Game.Renderables) {
+                        if (renderable is Racket racket) {
+                            racket.SetDirection(Direction.Right).Update();
+                        }
+                    }
+                    break;
+            }
+        }
         
     }
 }
