0e4937a58e993b0cd7c80b323a0f00454e09db8f
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-19 18:48:58 -0400

    Game test phase - with Racket and Brick

--- Breakout Game/Game/Game.cs
+++ Breakout Game/Game/Game.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Breakout_Game.Audio;
 using Breakout_Game.Game.Events;
 using Breakout_Game.Game.Forms;
 using Breakout_Game.Game.Levels;
 using Breakout_Game.Game.Text;
+using Breakout_Game.Game.Texture;
+using Breakout_Game.Game.UserInterac;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using ThreadState = System.Diagnostics.ThreadState;
 
 namespace Breakout_Game.Game{
     internal sealed class Game : GameBase{
         private static Game _gmInstance = null;
         private readonly GameWindow _gameWindow;
 
-        private Brick brick;
-        private Brick bricks;
-        private Brick brickss;
-        private Ball ball;
-        private Level first;
+        internal static List<IRenderable> Renderables = new List<IRenderable>();
 
+        private static int ActualLevelNumber = 0;
         
         internal static Game GetInstance (GameWindow gw){
             //?? -> is null
@@ -40,27 +42,22 @@ namespace Breakout_Game.Game{
             this._gameWindow.RenderFrame += this.Render;
             this._gameWindow.Resize += base.Resize;
 
+            this._gameWindow.KeyDown += UserControl.OnKeyDown;
+
             this._gameWindow.Run(1.0/60.0);
         }
 
         private void Load(object sender, EventArgs e){
             GL.ClearColor(.75f, .75f, .75f, 1.0f);
             GL.Enable(EnableCap.Texture2D);
-            
-            new Thread(AudioManager.init).Start(); 
             //TODO
-            this.brick = new Brick(new Vector2(-290, 140));
-            this.bricks = new Brick(new Vector2(-80.0f, -40.0f));
-            this.brickss = new Brick(new Vector2(-100, 100));
             
             TextManager.init();
+            new Thread((AudioManager.init)).Start();
+            LevelManager.GenerateFirstLevel();
             
-            LevelManager.CreateFirstLevel();
-            this.first = LevelManager.GetFirstLevel();
-
-
-
-
+            Renderables.Add(new Racket());
+            
 
         }
 
@@ -70,17 +67,18 @@ namespace Breakout_Game.Game{
         private void Render(object sender, EventArgs eventArgs){
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            foreach (List<Brick> firstBrick in this.first.bricks) {
+            foreach (IRenderable renderable in Renderables) {
+                renderable.Draw();
+            }
+
+            foreach (List<Brick> firstBrick in LevelManager._levels[ActualLevelNumber].bricks) {
                 foreach (Brick brick1 in firstBrick) {
                     brick1?.Draw();
                 }
             }
-            
-            TextManager.TestText.Draw();
-            
-            //this.brick.Draw();
 
             this._gameWindow.SwapBuffers();
+
         }
 
         public static void CallEvent(Event e){ //If an event called ...
