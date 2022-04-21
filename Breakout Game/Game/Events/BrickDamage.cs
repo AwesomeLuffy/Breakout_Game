891b65555c3ff5d0c907f9bce0a5518d832cba21
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-04-21 16:16:13 -0400

    Adding BrickDamage event

---
+++ Breakout Game/Game/Events/BrickDamage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Breakout_Game.Game.Forms;
+
+namespace Breakout_Game.Game.Events{
+    public sealed class BrickDamage : Event{
+        internal static event EventHandler Handler;
+        
+        internal Brick Brick{ get; private set; }
+        internal int DamageAmount{ get; private set; }
+        
+
+        internal BrickDamage(Brick brick, int amount){
+            this.Brick = brick;
+            this.DamageAmount = amount;
+            Handler?.Invoke(this, EventArgs.Empty);
+        }
+
+        public EventHandler GetHandler(){
+            return Handler;
+        }
+    }
+}
