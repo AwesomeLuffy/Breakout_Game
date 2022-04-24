9960f9f466a5a1de542fd27e6079606d656b364e
KISNOC <robinmcd56@gmail.com>
2022-04-24 16:43:05 -0400

    TextManager Done

--- Breakout Game/Game/Text/TextManager.cs
+++ Breakout Game/Game/Text/TextManager.cs
@@ -12,53 +12,21 @@ namespace Breakout_Game.Game.Text
     {
         private static IList<Text> Texts;
 
-        //public static readonly Text TestText = new Text(
-        //        220,
-        //        25,
-        //        "TXT HERE",
-        //        new PointF(0.0f, 0.0f)
-        //    );
-        //public static readonly Text TestDefaultText = new Text(
-        //        220,
-        //        25,
-        //        "OTHER TXT HERE",
-        //        new PointF(0.0f, -50.0f)
-        //    );
         public static readonly Text Text1 = new Text(
+                new Vector2(0, 0),    
                 220,
                 40,
-                "TXT 2",
-                new PointF(0.0f, 0.0f),
+                "text-1",
                 Color.Black,
                 new Font(FontFamily.GenericSansSerif, 11),
                 new SolidBrush(Color.Yellow)
             );
-        public static readonly Text Text2 = new Text(
-                220,
-                40,
-                "TXT 1",
-                new PointF(-250.0f, 0.0f),
-                Color.Black,
-                new Font(FontFamily.GenericSansSerif, 11),
-                new SolidBrush(Color.Yellow)
-            );
-        //public static readonly Text Text3 = new Text(
-        //        220,
-        //        40,
-        //        "TXT 3",
-        //        new PointF(-100.0f, -100.0f),
-        //        Color.Black,
-        //        new Font(FontFamily.GenericSansSerif, 11),
-        //        new SolidBrush(Color.Yellow)
-        //    );
 
         static TextManager()
         {
             Texts = new List<Text>()
             {
-                Text1,
-                Text2
-                //Text3
+                Text1
             };
         }
         public static void init() { }
