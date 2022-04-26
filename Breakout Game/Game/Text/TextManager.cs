49f73d70b10dada5155d339debc972c96c3e5bc3
KISNOC <robinmcd56@gmail.com>
2022-04-26 17:03:02 -0400

    Add text for delimitation test

--- Breakout Game/Game/Text/TextManager.cs
+++ Breakout Game/Game/Text/TextManager.cs
@@ -13,20 +13,44 @@ namespace Breakout_Game.Game.Text
         private static IList<Text> Texts;
 
         public static readonly Text Text1 = new Text(
-                new Vector2(0, 0),    
-                220,
-                40,
+                new Vector2(-100, 100),    
+                250,
+                5,
                 "text-1",
                 Color.Black,
                 new Font(FontFamily.GenericSansSerif, 11),
                 new SolidBrush(Color.Yellow)
             );
+        public static readonly Text Text2 = new Text(
+            new Vector2(-100, -100),    
+            250,
+            5,
+            "text-2",
+            Color.Black
+        );
+        public static readonly Text Text3 = new Text(
+            new Vector2(-100, -100),    
+            5,
+            290,
+            "text-3",
+            Color.Black
+        );
+        public static readonly Text Text4 = new Text(
+            new Vector2(100, -100),    
+            5,
+            290,
+            "text-4",
+            Color.Black
+        );
 
         static TextManager()
         {
             Texts = new List<Text>()
             {
-                Text1
+                Text1,
+                Text2,
+                Text3,
+                Text4
             };
         }
         public static void init() { }
