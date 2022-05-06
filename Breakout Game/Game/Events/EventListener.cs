e83930fa3796a44667d02c6f8f51dcd03408d099
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-06 17:54:54 -0400

    Changing events modifiers to private

--- Breakout Game/Game/Events/EventListener.cs
+++ Breakout Game/Game/Events/EventListener.cs
@@ -11,7 +11,7 @@ namespace Breakout_Game.Game.Events{
             BrickDestroyAnimationFinished.Handler += OnBrickDestroyAnimationFinished;
         }
 
-        public static void OnFormCreate(object sender, EventArgs e){
+        private static void OnFormCreate(object sender, EventArgs e){
             if (!(sender is CreateFormEvent form)) return;
             if (!(form.GetForm() is Brick)) return;
             Brick brick = form.GetForm() as Brick;
@@ -21,7 +21,7 @@ namespace Breakout_Game.Game.Events{
 
         }
 
-        public static void OnBrickDamage(object sender, EventArgs e){
+        private static void OnBrickDamage(object sender, EventArgs e){
             if (!(sender is BrickDamage brickDamage)) return;
             if (brickDamage.IsCancelled()) return;
             if (brickDamage.Brick.Level == 0) {
@@ -29,7 +29,7 @@ namespace Breakout_Game.Game.Events{
             }
         }
 
-        public static void OnBrickDestroyAnimationFinished(object sender, EventArgs e){
+        private static void OnBrickDestroyAnimationFinished(object sender, EventArgs e){
             if(!(sender is BrickDestroyAnimationFinished brickDestroyAnimationFinished)) return;
             foreach (var bricks in LevelManager._levels[Game.ActualLevelNumber].bricks) {
                 bricks.Remove(brickDestroyAnimationFinished.Brick);
