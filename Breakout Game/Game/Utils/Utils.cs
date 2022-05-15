c41d576683eede277f20bc853070ee869ae03653
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-15 14:37:22 -0400

    Adding IsPointIsInRect to check if a point is in an area

--- Breakout Game/Game/Utils/Utils.cs
+++ Breakout Game/Game/Utils/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using OpenTK;
 
 namespace Breakout_Game.Game.Utils{
     public static class Utils{
@@ -7,6 +8,11 @@ namespace Breakout_Game.Game.Utils{
         public static bool IsValueInListExists<T>(IEnumerable<T> list, T obj){
             //Check if object is in a list
             return list.Any(n => n.Equals(obj));
+        }
+
+        public static bool IsPointIsInRect(Vector2 rectBasePos, float rectWidth, float rectHeight, Vector2 pointToCheck){
+            return (pointToCheck.X >= rectBasePos.X && pointToCheck.X <= rectBasePos.X + rectWidth) &&
+                   (pointToCheck.Y >= rectBasePos.Y && pointToCheck.Y <= rectBasePos.Y + rectHeight);
         }
     }
 }
