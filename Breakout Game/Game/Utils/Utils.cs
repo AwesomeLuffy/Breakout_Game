243a499791bf4ff46bad9c051749cb0a3e096d2b
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-16 21:54:26 -0400

    Adding Map method to map mouse coordinate to gamewindow

--- Breakout Game/Game/Utils/Utils.cs
+++ Breakout Game/Game/Utils/Utils.cs
@@ -14,5 +14,14 @@ namespace Breakout_Game.Game.Utils{
             return (pointToCheck.X >= rectBasePos.X && pointToCheck.X <= rectBasePos.X + rectWidth) &&
                    (pointToCheck.Y >= rectBasePos.Y && pointToCheck.Y <= rectBasePos.Y + rectHeight);
         }
+
+        public static (int xMouse, int yMouse) Map(int x, int y){
+            int xMouse =
+                x * (GameBase.WindowOrthoMaxWidth - GameBase.WindowOrthoMinWidth) / Program.WindowWidth +
+                GameBase.WindowOrthoMinWidth;
+            int yMouse = -(y * (GameBase.WindowOrthoMaxHeight - GameBase.WindowOrthoMinHeight) / Program.WindowHeight +
+                         GameBase.WindowOrthoMinHeight);
+            return (xMouse, yMouse);
+        }
     }
 }
