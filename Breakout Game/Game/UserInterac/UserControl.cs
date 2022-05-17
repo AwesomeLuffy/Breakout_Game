858bae13f7220dfe7ffcf0cd05f32c902fc53c47
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-16 21:53:47 -0400

    Adding OnMouseDown event

--- Breakout Game/Game/UserInterac/UserControl.cs
+++ Breakout Game/Game/UserInterac/UserControl.cs
@@ -2,6 +2,7 @@
 using Breakout_Game.Game;
 using Breakout_Game.Game.Forms;
 using Breakout_Game.Game.Levels;
+using Breakout_Game.Game.Menu;
 using Breakout_Game.Game.Utils;
 using OpenTK;
 using OpenTK.Input;
@@ -10,13 +11,15 @@ namespace Breakout_Game.Game.UserInterac{
     internal static class UserControl{
 
         private static KeyboardState _keyboardState;
+        private static MouseEventArgs _mouseEventArgs;
 
         internal static void AnyKeyDown(){
             _keyboardState = Keyboard.GetState();
-            
+
             if (!(_keyboardState.IsAnyKeyDown)) {
-                return; 
+                return;
             }
+
             if (_keyboardState.IsKeyDown(Key.Right)) {
                 if (Game.IsGamePause) return;
                 foreach (var renderable in Game.Renderables) {
@@ -34,11 +37,37 @@ namespace Breakout_Game.Game.UserInterac{
                 }
             }
             else if (_keyboardState.IsKeyDown(Key.P) || _keyboardState.IsKeyDown(Key.Escape)) {
+                MenuManager.ChangeMenu();
                 Game.IsGamePause = (!Game.IsGamePause);
                 System.Threading.Thread.Sleep(100);
             }
             else if (_keyboardState.IsKeyDown(Key.Space)) {
                 Game.IsGameStarted = true;
+            }
+        }
+
+        internal static void OnMouseDown(object sender, MouseEventArgs args){
+            _mouseEventArgs = args;
+            var (xMouse, yMouse) = Utils.Utils.Map(_mouseEventArgs.X, _mouseEventArgs.Y);
+            
+            if (args.Mouse.IsButtonDown(MouseButton.Left)) {
+
+                Log.Send("UserControl", "Left Mouse Button clicked !", LogType.Info);
+                if (Game.IsGamePause || !Game.IsGameStarted) {
+                    int i = 0;
+                    foreach (var actualMenuText in MenuManager.ActualMenu.Texts) {
+                        if (Utils.Utils.IsPointIsInRect(
+                                actualMenuText.getPosition(),
+                                actualMenuText.getWidth(),
+                                actualMenuText.getHeight(), new Vector2(xMouse, yMouse))) {
+                            MenuManager.ActualMenu.DoAction(i);
+                        }
+
+                        i++;
+                    }
+                }
+
+                System.Threading.Thread.Sleep(300);
             }
         }
 
