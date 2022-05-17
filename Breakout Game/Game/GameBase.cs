7d5ef495f1d79ad87599451c744e913ef425af2a
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-16 21:54:43 -0400

    Adding constant for window sizes

--- Breakout Game/Game/GameBase.cs
+++ Breakout Game/Game/GameBase.cs
@@ -5,6 +5,10 @@ using OpenTK.Graphics.OpenGL;
 namespace Breakout_Game.Game{
     public abstract class GameBase{
         private readonly GameWindow GameWindow;
+        public const int WindowOrthoMinWidth = -300;
+        public const int WindowOrthoMaxWidth = 300;
+        public const int WindowOrthoMinHeight = -150;
+        public const int WindowOrthoMaxHeight = 150;
 
         protected GameBase(GameWindow gameWindow){
             this.GameWindow = gameWindow;
@@ -14,7 +18,7 @@ namespace Breakout_Game.Game{
             GL.Viewport(0, 0, this.GameWindow.Width, this.GameWindow.Height);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Ortho(-300.0, 300.0, -150.0, 150, -1.0, 1.0);
+            GL.Ortho(WindowOrthoMinWidth, WindowOrthoMaxWidth, WindowOrthoMinHeight, WindowOrthoMaxHeight, -1.0, 1.0);
             GL.MatrixMode(MatrixMode.Modelview);
         }
 
--- Breakout Game/Program.cs
+++ Breakout Game/Program.cs
@@ -3,11 +3,13 @@ using OpenTK.Graphics;
 
 namespace Breakout_Game{
     internal static class Program{
+        public const int WindowWidth = 1200;
+        public const int WindowHeight = 600;
         public static void Main(string[] args){
-            
+
             Game.Game.GetInstance(new GameWindow(
-                1200,
-                600,
+                WindowWidth,
+                WindowHeight,
                 GraphicsMode.Default,
                 "Breakout Game")
             );
