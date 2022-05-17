6ba5506acb907434afc5f366cedd5277037b819b
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-17 19:55:42 -0400

    Adding Warn message for conversion coordinate when mouse clicked

--- Breakout Game/Game/GameBase.cs
+++ Breakout Game/Game/GameBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Breakout_Game.Game.Utils;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
@@ -20,6 +21,9 @@ namespace Breakout_Game.Game{
             GL.LoadIdentity();
             GL.Ortho(WindowOrthoMinWidth, WindowOrthoMaxWidth, WindowOrthoMinHeight, WindowOrthoMaxHeight, -1.0, 1.0);
             GL.MatrixMode(MatrixMode.Modelview);
+            Log.Send("GameBase",
+                "Becareful, depending of your device, the mouse click coordinate should not work correctly and menu will not be as exact position.",
+                LogType.Warn);
         }
 
 
