959e1b376b83b660e17cff8ca99470843f01e264
AwesomeLuffy <alpha.nexus.280@gmail.com>
2022-05-16 21:52:52 -0400

    Adding "List<>Texts" attributs

--- Breakout Game/Game/Menu/IMenu.cs
+++ Breakout Game/Game/Menu/IMenu.cs
@@ -1,6 +1,9 @@
-﻿using Breakout_Game.Game.Forms;
+﻿using System.Collections.Generic;
+using Breakout_Game.Game.Forms;
 
 namespace Breakout_Game.Game.Menu{
     internal interface IMenu : IDrawable{
+        abstract List<Text.Text> Texts{ get; set; }
+        void DoAction(int textPosition);
     }
 }
